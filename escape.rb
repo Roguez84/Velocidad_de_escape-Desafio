eb08f62a0449af33677a434e282f2d30c91a8e98
Roguez84 <t.roguez984@gmail.com>
2021-01-10 19:50:13 -0300

    finished desafio

--- escape.rb
+++ escape.rb
@@ -1,10 +1,16 @@
-puts "Ingrese gravedad del planeta"
-g = gets.chomp.to_f
 
-puts"Ingrese radio del planeta (en metros)"
-r = gets.chomp.to_f
 
-v_de_escape = (2*g*r)**0.5
+#puts "Ingrese gravedad del planeta"
+#g = gets.chomp.to_f
+g = ARGV[0].to_f
+
+#puts"Ingrese radio del planeta (en metros)"
+#r = gets.chomp.to_f
+r = ARGV[1].to_f
+
+#v_de_escape = (2*g*r)**0.5
+v_de_escape = Math.sqrt(2*g*r)
+
 puts "la velocidad de escape es #{v_de_escape}"
 
 ## Hay que cambiar esto para que el usuario use ARGV
